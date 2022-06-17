b23add77c643778a6e2b56999f4d4d7c17f7d996
Magali Morin <magali.morin1@hotmail.fr>
2022-06-17 16:43:18 +0200

    control change commitment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import { useState } from 'react';
 
 function App() {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
+  console.log(word)
+
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title='image gallery' />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: 'red',
+}
+
+const Header = (props) => {
+    const { title } = props;
+    return (<Navbar style={navbarStyle} variant="light">
+        <Container>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+        </Container>
+    </Navbar>)
+}
+
+export default Header;
